41896ab48df8969d8db6a598f20521d52bdc5fed
bogdanaKolic <108514766+bogdanaKolic@users.noreply.github.com>
2022-07-11 15:51:59 +0200

    Update code.py

--- code.py
+++ code.py
@@ -2,6 +2,7 @@
 
 import random
 import math
+import collections
 
 max_coeff = 100 # maximal generated coefficient of a monomial
 max_power = 100 # maximal generated power of a variable in a monomial
@@ -75,24 +76,42 @@ class Monomial:
         return product
     
     def __add__(self, other):
-        if self.origin != None:
+        if not (self.origin is None):
             self.is_currently_present = self.origin.is_currently_present
-        if other.origin != None:
+        if not (other.origin is None):
             other.is_currently_present = other.origin.is_currently_present
         return self.is_currently_present or other.is_currently_present
     
     def __mul__(self, other):
-        if self.origin != None:
+        if not (self.origin is None):
             self.is_currently_present = self.origin.is_currently_present
-        if other.origin != None:
+        if not (other.origin is None):
             other.is_currently_present = other.origin.is_currently_present
         return self.is_currently_present and other.is_currently_present
     
     def __hash__(self):
         return hash(self.variables)
     
-    def __str__(self):
+    def __repr__(self):
         return str(self.coefficient) + '*' + str(self.variables)      
+    
+    def __str__(self, use_coefficient = True):
+        if use_coefficient:
+            s = str(self.coefficient)
+        else:
+            s = ''
+        if self.variables != (0, ) * self.width:
+            s += ' * ('
+            for i, power in enumerate(self.variables):
+                if power > 0:
+                    s += f'x{i}^{power} * '
+                elif power < 0:
+                    s += f'x{i}^({power}) * '
+            s = s.strip(' *')
+            s += ')'
+        if s == '':
+            s = '1'
+        return s
     
 class Laurent(Monomial):
     """Laurent monomials in addition have the index of the variable by which 
@@ -119,8 +138,8 @@ class Laurent(Monomial):
         return product
     
     def __str__(self):
-        s = super().__str__()
-        s += '\nderivative: ' + str(self.derivative) + '\nindex: ' + str(self.index)
+        s = super().__str__(False)
+        #s += '\nderivative: ' + str(self.derivative) + '\nindex: ' + str(self.index)
         return s
 
 class Polynomial:
@@ -209,9 +228,8 @@ class Equation(Polynomial):
     def __str__(self):
         s = f'x{self.index} = ' + super().__str__()
         return s
-
-# Maybe rename to QuadratizationProblem
-class Test():
+    
+class QuadratizationProblem():
     """ Class for performing single test - either on a benchmark set or 
     generates a random set of equations and tries to minimize the number of 
     Laurent monomials used in quadratization """
@@ -232,8 +250,6 @@ class Test():
         for e in self.equations:
             self.all_substitutions += e.calculate_substitutions(self.equations)
         self.all_substitutions = list(set(self.all_substitutions))
-        self.optimal_solution = [y for y in self.all_substitutions]
-        self.min_length = len(self.optimal_solution)
         # pre-compute the dictionary of products represented as at most quadratic
         # monomials appearing in the system with all the substitutions
         search_space_deg1 = set()
@@ -285,19 +301,18 @@ class Test():
     def find_must_have_substitutions(self):
         """ Find out which substitutions have to be present in the system 
         and compute the list of optional substitutions"""
-        check = {monom for monom in self.product_results if monom.origin is None}
-        newly_found_substitutions = set()
-        # Exercise: have just while-loop by using collections.Deque or list
+        check = collections.deque()
+        for monom in self.product_results:
+            if monom.origin is None:
+                check.append(monom)
         while len(check) != 0:
-            for monom in check:
-                if len(self.product_results[monom]) == 1:
-                    for m in self.product_results[monom][0]:
-                        if not m.is_always_present and m.origin is not None:
-                            newly_found_substitutions.add(m)
-                        m.is_currently_present = True
-                        m.is_always_present = True
-            check = newly_found_substitutions.copy()
-            newly_found_substitutions.clear()
+            monom = check.popleft()
+            if len(self.product_results[monom]) == 1:
+                for m in self.product_results[monom][0]:
+                    if not m.is_always_present and m.origin is not None:
+                        check.append(m)
+                    m.is_currently_present = True
+                    m.is_always_present = True
         for substitution in self.all_substitutions:
             if substitution.is_always_present:
                 self.current_length += 1
@@ -411,7 +426,7 @@ class Test():
     
     def run(self):
         """ Search for the quadratization """
-        #print(self)
+        # print(self)
         non_quadratized = self.precompute_non_quadratized() # non-quadratized monomials
         self.reduce(0, non_quadratized)
         for substitution in self.all_substitutions:
@@ -419,7 +434,7 @@ class Test():
                 self.optimal_solution.append(substitution)
         # print('Solution:')
         # for laurent in self.optimal_solution:
-        #    print(laurent.variables)
+        #    print(laurent)
         print('optimal number of substitutions: ', self.min_length)
         print('number of all substitutions: ', len(self.all_substitutions), '\n')
         
@@ -436,276 +451,12 @@ class Test():
 def main_random():
     """ Simulate quadratizing a set of randomly generated equations """
     for _ in range(10):
-        t = Test()
+        t = QuadratizationProblem()
         t.random_test()
-        t.run()
-
-# following functions were used for testing the first algorithm     
+        t.run()  
 
 def main_from_file():
     """ Perform a test on the system stored in a file"""
-    t = Test()
+    t = QuadratizationProblem()
     t.load_from_file('cubic_bicycle(7).txt')
     t.run()
-
-# TODO: put into "examples.py" or "benchmarks.py" file
-
-def generate_circular_test():
-    """Create an instance of class Test that represents a circular system 
-    of ODEs"""
-    width = random.randint(1, 15) # n
-    degree = random.randint(1, 50) # k
-    equations = []
-    l = [0]*width
-    for i in range(width):
-        l[(i + 1) % width] = degree
-        monom = Monomial(width, 1, tuple(l))
-        equations.append(Equation(i, width, 1, [monom]))
-        l[(i + 1) % width] = 0
-    return Test(width, equations)
-    
-
-def circular_benchmark_tests(repeat):
-    """Perform a series of tests on circular systems of ODEs, for a given
-    number of repetitions"""
-    with open('circular_benchmark_tests.txt', 'a') as outfile:
-        for i in range(repeat):
-            outfile.write(f'Test {i + 1}:\n')
-            test = generate_circular_test()
-            outfile.write(str(test))
-            test.run()
-            outfile.write('\nSoultion:\n')
-            for laurent in test.optimal_solution:
-                outfile.write(str(laurent.variables))
-                outfile.write('\n')
-            outfile.write('optimal number of substitutions: ')
-            outfile.write(str(test.min_length))
-            outfile.write('\nnumber of all substitutions: ')
-            outfile.write(str(len(test.all_substitutions))) 
-            outfile.write('\n\n')
-            
-def generate_hardk_test():
-    """Create an instance of class Test that represents a hardk system 
-    of ODEs, return it and the degree k of the system"""
-    width = 3 # variables A(index 0), B(index 1) and C(index 2)
-    degree = random.randint(1, 50) # k
-    monomials_of_A = [Monomial(width, 1, (0, 0, degree)), Monomial(width, 1, (2, 2, degree - 1))]
-    eqA = Equation(0, width, 2, monomials_of_A)
-    monomials_of_B = [Monomial(width, 1, (2, 0, 0))]
-    eqB = Equation(1, width, 1, monomials_of_B)
-    monomials_of_C = [Monomial(width, 1, (0, 2, 0))]
-    eqC = Equation(2, width, 1, monomials_of_C)
-    return Test(width, [eqA, eqB, eqC]), degree
-
-def hardk_benchmark_tests(repeat):
-    """Perform a series of tests on hardk systems of ODEs, for a given
-    number of repetitions"""
-    with open('hardk_benchmark_tests.txt', 'a') as outfile:
-        for i in range(repeat):
-            outfile.write(f'Test {i + 1}:\n')
-            test, k = generate_hardk_test()
-            outfile.write(f'Degree k: {k}\n')
-            outfile.write(str(test))
-            test.run()
-            outfile.write('\nSolution:\n')
-            for laurent in test.optimal_solution:
-                outfile.write(str(laurent.variables))
-                outfile.write('\n')
-            outfile.write('optimal number of substitutions: ')
-            outfile.write(str(test.min_length))
-            outfile.write('\nnumber of all substitutions: ')
-            outfile.write(str(len(test.all_substitutions))) 
-            outfile.write('\n\n')
-            
-def generate_monomn_test():
-    """Create an instance of class Test that represents a monomn system 
-    of ODEs"""
-    width = random.randint(1, 9)
-    equations = []
-    common_monomial = Monomial(width, 1, (2, ) * width)
-    l = [0] * width
-    for i in range(width):
-        l[(i + 1) % width] = 2
-        monom = Monomial(width, 1, tuple(l))
-        l[(i + 1) % width] = 0
-        equations.append(Equation(i, width, 2, [common_monomial, monom]))
-    return Test(width, equations)
-
-def monomn_benchmark_tests(repeat):
-    """Perform a series of tests on monomn systems of ODEs, for a given
-    number of repetitions"""
-    with open('monom_benchmark_tests.txt', 'a') as outfile:
-        for i in range(repeat):
-            outfile.write(f'Test {i + 1}:\n')
-            test = generate_monomn_test()
-            outfile.write(str(test))
-            test.run()
-            outfile.write('\nSolution:\n')
-            for laurent in test.optimal_solution:
-                outfile.write(str(laurent.variables))
-                outfile.write('\n')
-            outfile.write('optimal number of substitutions: ')
-            outfile.write(str(test.min_length))
-            outfile.write('\nnumber of all substitutions: ')
-            outfile.write(str(len(test.all_substitutions))) 
-            outfile.write('\n\n')
-
-def generate_hilln_tests():
-    """Create two instances of class Test that represent hill systems
-    of ODEs, one with 3, and one with 4 variables; return them and their order"""
-    order = random.randint(1, 100) # n
-    width = 3 # variables H(index 0), I(index 1) and T(index 2)
-    monom_H_3 = Monomial(width, order, (0, 2, order - 1))
-    monom_I_3 = Monomial(width, - order, (0, 2, order - 1))
-    monom_T_3 = Monomial(width, 1, (0, 0, 0))
-    equation_H_3 = Equation(0, width, 1, [monom_H_3])
-    equation_I_3 = Equation(1, width, 1, [monom_I_3])
-    equation_T_3 = Equation(2, width, 1, [monom_T_3])
-    test_3 = Test(width, [equation_H_3, equation_I_3, equation_T_3])
-    width = 4 # variables H(index 0), I(index 1) and T(index 2) and X(index 4)
-    monom_H_4 = Monomial(width, order, (0, 2, order - 1, 1))
-    monom_I_4 = Monomial(width, - order, (0, 2, order - 1, 1))
-    monom_T_4 = Monomial(width, 1, (0, 0, 0, 1))
-    monom_X_4 = Monomial(width, -1, (0, 0, 0, 1))
-    equation_H_4 = Equation(0, width, 1, [monom_H_4])
-    equation_I_4 = Equation(1, width, 1, [monom_I_4])
-    equation_T_4 = Equation(2, width, 1, [monom_T_4])
-    equation_X_4 = Equation(3, width, 1, [monom_X_4])
-    test_4 = Test(width, [equation_H_4, equation_I_4, equation_T_4, equation_X_4])
-    return test_3, test_4, order
-
-def hilln_benchmark_tests(repeat):
-    """Perform a series of tests on hilln systems of ODEs (with both 3 and 4
-    variables), for a given number of repetitions"""
-    with open('hilln_benchmark_tests.txt', 'a') as outfile:
-        for i in range(repeat):
-            outfile.write(f'Test {i + 1}:\n')
-            test_3, test_4, order = generate_hilln_tests()
-            outfile.write(f'Order n: {order}\n')
-            outfile.write('\nWith 3 variables:\n')
-            outfile.write(str(test_3))
-            test_3.run()
-            outfile.write('\nSolution:\n')
-            for laurent in test_3.optimal_solution:
-                outfile.write(str(laurent.variables))
-                outfile.write('\n')
-            outfile.write('optimal number of substitutions: ')
-            outfile.write(str(test_3.min_length))
-            outfile.write('\nnumber of all substitutions: ')
-            outfile.write(str(len(test_3.all_substitutions)))
-            outfile.write('\n\nWith 4 variables:\n')
-            outfile.write(str(test_4))
-            test_4.run()
-            outfile.write('\nSolution:\n')
-            for laurent in test_4.optimal_solution:
-                outfile.write(str(laurent.variables))
-                outfile.write('\n')
-            outfile.write('optimal number of substitutions: ')
-            outfile.write(str(test_4.min_length))
-            outfile.write('\nnumber of all substitutions: ')
-            outfile.write(str(len(test_4.all_substitutions))) 
-            outfile.write('\n\n')
-    
-def generate_selkov_test():
-    """Create an instance of class Test that represents a selkov system 
-    of ODEs, return it and the parameters a and b of the system"""
-    width = 2 # two variables X(index 0) and Y(index 1)
-    a = random.randint(-100, 100)
-    b = random.randint(-100, 100)
-    monomials_X = [Monomial(width, -1, (1, 0)), Monomial(width, a, (0, 1)), Monomial(width, 1, (2, 1))]
-    monomials_Y = [Monomial(width, b, (0, 0)), Monomial(width, - a, (0, 1)), Monomial(width, -1, (2, 1))]
-    eqX = Equation(0, width, 3, monomials_X)
-    eqY = Equation(1, width, 3, monomials_Y)
-    return Test(width, [eqX, eqY]), a, b
-
-def selkov_benchmark_tests(repeat):
-    """Perform a series of tests on selkov systems of ODEs, for a given number
-    of repetitions"""
-    with open('selkov_benchmark_tests.txt', 'a') as outfile:
-        for i in range(repeat):
-            outfile.write(f'Test {i + 1}:\n')
-            test, a, b = generate_selkov_test()
-            outfile.write(f'parameters: a = {a}, b = {b}\n')
-            outfile.write(str(test))
-            test.run()
-            outfile.write('\nSoultion:\n')
-            for laurent in test.optimal_solution:
-                outfile.write(str(laurent.variables))
-                outfile.write('\n')
-            outfile.write('optimal number of substitutions: ')
-            outfile.write(str(test.min_length))
-            outfile.write('\nnumber of all substitutions: ')
-            outfile.write(str(len(test.all_substitutions))) 
-            outfile.write('\n\n')
-        
-def generate_cubic_cycle_test():
-    """Create an instance of class Test that represents a cubic cycle system 
-    of ODEs"""
-    width = random.randint(1, 15)
-    equations = []
-    l = [0] * width
-    for i in range(width):
-        l[(i + 1) % width] = 3
-        monom = Monomial(width, 1, tuple(l))
-        l[(i + 1) % width] = 0
-        equations.append(Equation(i, width, 1, [monom]))
-    return Test(width, equations)
-
-def cubic_cycle_benchmark_tests(repeat):
-    """Perform a series of tests on cubic cycle systems of ODEs, for a given 
-    number of repetitions"""
-    with open('cubic_cycle_benchmark_tests.txt', 'a') as outfile:
-        for i in range(repeat):
-            outfile.write(f'Test {i + 1}:\n')
-            test = generate_cubic_cycle_test()
-            outfile.write(str(test))
-            test.run()
-            outfile.write('\nSoultion:\n')
-            for laurent in test.optimal_solution:
-                outfile.write(str(laurent.variables))
-                outfile.write('\n')
-            outfile.write('optimal number of substitutions: ')
-            outfile.write(str(test.min_length))
-            outfile.write('\nnumber of all substitutions: ')
-            outfile.write(str(len(test.all_substitutions))) 
-            outfile.write('\n\n')
-            
-def generate_cubic_bicycle_test():
-    """Create an instance of class Test that represents a cubic bicycle system 
-    of ODEs"""
-    width = random.randint(1, 20)
-    equations = []
-    l = [0] * width
-    for i in range(width):
-        l[i - 1] = 3
-        monom_left = Monomial(width, 1, tuple(l))
-        l[i - 1] = 0
-        l[(i + 1) % width] = 3
-        monom_right = Monomial(width, 1, tuple(l))
-        l[(i + 1) % width] = 0
-        equations.append(Equation(i, width, 2, [monom_left, monom_right]))
-    return Test(width, equations)
-
-def cubic_bicycle_benchmark_tests(repeat):
-    """Perform a series of tests on cubic bicycle systems of ODEs, for a given 
-    number of repetitions"""
-    with open('cubic_bicycle_benchmark_tests.txt', 'a') as outfile:
-        for i in range(repeat):
-            outfile.write(f'Test {i + 1}:\n')
-            test = generate_cubic_bicycle_test()
-            outfile.write(str(test))
-            test.run()
-            outfile.write('\nSoultion:\n')
-            for laurent in test.optimal_solution:
-                outfile.write(str(laurent.variables))
-                outfile.write('\n')
-            outfile.write('optimal number of substitutions: ')
-            outfile.write(str(test.min_length))
-            outfile.write('\nnumber of all substitutions: ')
-            outfile.write(str(len(test.all_substitutions))) 
-            outfile.write('\n\n')
-    
-    
-
-    
-      
